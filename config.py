bd2d73f483b5d6fee990d5a1768cd4b6e5db3224
kiran7368 <kiranbabukaligithi7148@gmail.com>
2026-05-30 23:13:33 +0530

    deploy fix

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
+import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:736852@127.0.0.1:5432/productivity_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://postgres1:Cj84E5QgJOlyMwjIcFEeJG6jgipv2xVt@dpg-d8dha0q8qa3s739nk9c0-a/productivity_db_jsd0")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'supersecretkey'
