72af6f2050868bb60b99a1a8d9d089405d0ad034
kiran7368 <kiranbabukaligithi7148@gmail.com>
2026-06-01 11:46:43 +0530

    final working version

--- config.py
+++ config.py
@@ -3,7 +3,12 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
-    if SQLALCHEMY_DATABASE_URI:
+    # If running locally → use local DB
+    if not SQLALCHEMY_DATABASE_URI:
+        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:736852@127.0.0.1:5432/productivity_db"
+
+    # Fix for Render
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
